6aa03726d0d5bd72ce96fa24cfdb7192a0d162e2
Mark Angelo Guerra <14197001+mark-war@users.noreply.github.com>
2025-02-23 10:30:59 +0400

    ADDED USER HISTORY

--- migrations/V5__add-user.sql
+++ migrations/V5__add-user.sql
@@ -5,9 +5,12 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+
+-- Create Users table with a primary key on the Id column
 CREATE TABLE [dbo].[Users](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[UserName] [nvarchar](max) NOT NULL,
-	[IsActive] [bit] NOT NULL,
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [UserName] [nvarchar](max) NOT NULL,
+    [IsActive] [bit] NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])  -- Define primary key on Id column
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
 GO

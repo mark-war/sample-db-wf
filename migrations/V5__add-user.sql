a07f474d97f1e9482164c42da8a52b4362518ade
Mark Angelo Guerra <14197001+mark-war@users.noreply.github.com>
2025-02-23 10:41:42 +0400

    SCRIPT CHANGES

--- migrations/V5__add-user.sql
+++ migrations/V5__add-user.sql
@@ -11,6 +11,19 @@ CREATE TABLE [dbo].[Users](
     [Id] [int] IDENTITY(1,1) NOT NULL,
     [UserName] [nvarchar](max) NOT NULL,
     [IsActive] [bit] NOT NULL,
-    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])  -- Define primary key on Id column
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY];
-GO
+    CONSTRAINT [PK_Users] PRIMARY KEY ([Id])
+);
+
+-- Create UserHistory table
+CREATE TABLE [dbo].[UserHistory] (
+    [HistoryId] [int] IDENTITY(1,1) NOT NULL,
+    [UserId] [int] NOT NULL,
+    [UserName] [nvarchar](max) NOT NULL,
+    [IsActive] [bit] NOT NULL,
+    [ChangeType] [nvarchar](max) NOT NULL,
+    [ChangedAt] [datetime] NOT NULL DEFAULT GETDATE(),
+    [ChangedBy] [nvarchar](max) NULL,
+    CONSTRAINT [PK_UserHistory] PRIMARY KEY ([HistoryId]),
+    CONSTRAINT [FK_UserHistory_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users]([Id]),
+    CONSTRAINT [CK_UserHistory_ChangeType] CHECK ([ChangeType] IN ('INSERT', 'UPDATE', 'DELETE'))
+);
--- migrations/V6__add-history.sql
+++ migrations/V6__add-history.sql
@@ -1,21 +1,7 @@
--- V6__add_user_history_table.sql
-
--- Creating the UserHistory table
-CREATE TABLE [dbo].[UserHistory] (
-    [HistoryId] [int] IDENTITY(1,1) NOT NULL,
-    [UserId] [int] NOT NULL,
-    [UserName] [nvarchar](max) NOT NULL,
-    [IsActive] [bit] NOT NULL,
-    [ChangeType] [nvarchar](max) NOT NULL,
-    [ChangedAt] [datetime] NOT NULL DEFAULT GETDATE(), -- Timestamp of the change
-    [ChangedBy] [nvarchar](max) NULL,  -- Optional: could store user who made the change
-    CONSTRAINT [PK_UserHistory] PRIMARY KEY ([HistoryId]),
-    CONSTRAINT [FK_UserHistory_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users]([Id]),
-    CONSTRAINT [CK_UserHistory_ChangeType] CHECK ([ChangeType] IN ('INSERT', 'UPDATE', 'DELETE')) -- Ensuring only 'INSERT', 'UPDATE', or 'DELETE' values
-) ON [PRIMARY];
+USE [ArgaamScreener_DEV];
 GO
 
--- Trigger for INSERT on Users table
+-- Create triggers after tables have been created
 CREATE TRIGGER trg_User_Insert
 ON [dbo].[Users]
 AFTER INSERT
@@ -24,10 +10,9 @@ BEGIN
     INSERT INTO [dbo].[UserHistory] ([UserId], [UserName], [IsActive], [ChangeType], [ChangedAt], [ChangedBy])
     SELECT [Id], [UserName], [IsActive], 'INSERT', GETDATE(), NULL
     FROM inserted;
-END
+END;
 GO
 
--- Trigger for UPDATE on Users table
 CREATE TRIGGER trg_User_Update
 ON [dbo].[Users]
 AFTER UPDATE
@@ -36,10 +21,9 @@ BEGIN
     INSERT INTO [dbo].[UserHistory] ([UserId], [UserName], [IsActive], [ChangeType], [ChangedAt], [ChangedBy])
     SELECT [Id], [UserName], [IsActive], 'UPDATE', GETDATE(), NULL
     FROM inserted;
-END
+END;
 GO
 
--- Trigger for DELETE on Users table
 CREATE TRIGGER trg_User_Delete
 ON [dbo].[Users]
 AFTER DELETE
@@ -48,5 +32,5 @@ BEGIN
     INSERT INTO [dbo].[UserHistory] ([UserId], [UserName], [IsActive], [ChangeType], [ChangedAt], [ChangedBy])
     SELECT [Id], [UserName], [IsActive], 'DELETE', GETDATE(), NULL
     FROM deleted;
-END
+END;
 GO
